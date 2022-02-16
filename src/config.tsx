335cfdf800cb4526cf8bd086f980dffeccabe442
fintaovidiu <ovidiu_adrian.finta@stud.acs.upb.ro>
2022-02-16 21:40:05 +0200

    setup

--- src/config.tsx
+++ src/config.tsx
@@ -1,4 +1,4 @@
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgquvt728n40ssd8n2qns9jrlqpwq2jc4rj4cysfuj3ad';
+  'erd1qqqqqqqqqqqqqpgqhvfrsrel4auhcc6ylwaejj54825apr6tzgpqs7aqwn';
 
 export const dAppName = 'example Dapp';
